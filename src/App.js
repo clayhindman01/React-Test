9d03e94af8194b06d62a16675d1d4875cfb67cd5
Clay Hindman <clayhindman01@gmail.com>
2021-07-22 15:59:16 -0400

    Changes made to the header and overall layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Main from "./Main"
 import Header from "./Header.js"
+import Footer from "./Footer.js"
 
 function App() {
   return (
     <div className="App">
       <Header className="App-header" />
-      <Main />
+      <Main className="Main-content"/>
+      <Footer className="Footer" />
     </div>
   );
 }
